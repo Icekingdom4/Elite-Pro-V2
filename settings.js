87a8e1b707d324c44a8ea8cc201165165235cbd1
Icekingdom4 <ronniebigrigsale@gmail.com>
2024-09-20 15:20:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,8 @@ global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
+global.botname = 'ÎcEkîÑGrÁDËR'//ur bot name
+global.ownernumber = '2349095894070' //ur owner number
 global.ownername = 'ELITE-PRO-V2' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
